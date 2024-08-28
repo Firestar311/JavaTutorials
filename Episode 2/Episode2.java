939de1812ced53e9a79384daa77b0d53ab9e7e06
Firestar311 <slaterandrew0398@gmail.com>
2024-08-27 21:26:58 -0500

    Episode 2 Code

---
+++ Episode 2/Episode2.java
@@ -0,0 +1,23 @@
+public class Episode2 {
+	public static void main(String[] args) {
+		// 8 primitive data types
+		// Integer Types (Whole Numbers): [int] [long] byte short
+		// Floating Types (Decimal Numbers): [double] float
+		// char type - Single UTF-16 character or short code
+		// boolean type - Only valid values are true or false true = 1 and false = 0
+		
+		//String is a collection of characters
+		
+		// A variable is a place in memory reserved for data
+		
+		int x = 20;
+		double y = 50.6;
+		char c = 'A';
+		boolean b = true;
+		
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("c = " + c);
+		System.out.println("b = " + b);
+	}
+}
